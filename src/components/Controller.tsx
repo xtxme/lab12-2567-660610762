44768460668d0b9877c63a8ce36878894963212e
xtxme <TT.icy013@gmail.com>
2024-08-31 17:40:59 +0700

    Change Theme Complete

--- src/components/Controller.tsx
+++ src/components/Controller.tsx
@@ -2,26 +2,27 @@ import { LangContext } from "@contexts/LangContext";
 import { ThemeContext } from "@contexts/ThemeContext";
 import { useContext } from "react";
 
+
 export default function Controller() {
-  const { toggleTheme, setDarkTheme, setLightTheme } = useContext(ThemeContext);
-  const { toggleLang } = useContext(LangContext);
+  const { toggleTheme, setDarkTheme, setLightTheme,theme } = useContext(ThemeContext);
+  const { toggleLang,lang } = useContext(LangContext);
   return (
     <div className="d-flex justify-content-center gap-2">
-      <button className="btn btn-secondary" onClick={setLightTheme}>
+      <button className="btn btn-secondary" onClick={setLightTheme} disabled={theme.name==="light"}>
         {/* Please update this text so it shows in EN or TH */}
-        Light
+        {lang.buttons.setLightTheme}
       </button>
-      <button className="btn btn-dark" onClick={setDarkTheme}>
+      <button className="btn btn-dark" onClick={setDarkTheme} disabled={theme.name==="dark"}>
         {/* Please update this text so it shows in EN or TH */}
-        Light Dark
+        {lang.buttons.setDarkTheme}
       </button>
       <button className="btn btn-primary" onClick={toggleTheme}>
         {/* Please update this text so it shows in EN or TH */}
-        Light Toggle Theme
+        {lang.buttons.toggleTheme}
       </button>
       <button className="btn btn-danger" onClick={toggleLang}>
         {/* Please update this text so it shows in EN or TH */}
-        Light Toggle Language
+        {lang.buttons.toggleLang}
       </button>
     </div>
   );
