3f67b53f50313499254a87b9fb2b076f05ab436f
xtxme <TT.icy013@gmail.com>
2024-08-31 17:45:10 +0700

    Detail Complete

--- src/components/Detail.tsx
+++ src/components/Detail.tsx
@@ -1,7 +1,10 @@
 import { ThemeContext } from "@contexts/ThemeContext";
 import { useContext } from "react";
+import { LangContext } from "@contexts/LangContext";
+
 
 export const Detail = () => {
+  const { lang } = useContext(LangContext);
   const { theme } = useContext(ThemeContext);
   return (
     <div>
@@ -9,6 +12,7 @@ export const Detail = () => {
         Display activated theme message here (EN or TH)
         {/* tip: Get information from "theme" variable to determine 
     which theme is currently selected */}
+      {theme.name==="light"? lang.detail.lightActivated : lang.detail.darkActivated}
       </p>
     </div>
   );
